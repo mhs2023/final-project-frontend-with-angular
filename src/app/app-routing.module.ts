252ee8bef26461f0cfc41b62968b2bd6657f650d
mhs2023 <mh.ju1803@gmail.com>
2024-01-20 19:03:36 +0600

    room segment added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'room', component: RoomsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'createroom', component: CreateRoomComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    CreateRoomComponent
   ],
   imports: [
     BrowserModule,
